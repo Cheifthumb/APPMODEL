79aefc9ba07aa3cd6a44f2ad5131aa90db7d3291
Cheifthumb <JJTThursby@yahoo.com>
2025-07-30 22:19:16 +0100

    Initial commit for  APP model

--- config/track_betting_config.py
+++ config/track_betting_config.py
@@ -9,7 +9,8 @@ track_model_map = {
     "WINDSOR": "WINDSOR_model",
     "RIPON": "RIPON_model",
     "REDCAR": "REDCAR_model",
-    "NEWTON ABBOT": "NEWTON_ABBOT_model"
+    "NEWTON ABBOT": "NEWTON_ABBOT_model",
+    "EPSOM": "EPSOM_model"
     # Add more as needed
 }
 
@@ -94,6 +95,29 @@ track_betting_config = {
             "stake_mode": "split_half"   
      
         }
+      },
+    "EPSOM": {
+        "single": {
+            "bankroll_perc": 0.1,
+            "min_ev_threshold": 0.00,
+            "min_kelly_fraction": 5,
+            "max_odds_threshold": 15.1,
+            "min_odds_threshold": 5.01,
+            "stake_mode": "kelly",              # 'kelly', 'fixed', or 'winrate'
+            "fixed_stake_perc": 0.01,
+            "winrate_filter_type": "none",      # 'none', 'fixed', or 'dynamic'
+            "fixed_winrate_threshold": 0.03,
+            "allowed_predicted_ranks": [2,4],
+            "allowed_field_sizes": [7],
+        },
+        "reverse_forecast_a": {
+            "allowed_field_sizes": [6],
+            "allowed_predicted_ranks": [1,4],
+            "min_ev_threshold": -5.0,
+            "min_kelly_fraction": -1,
+            "stake_mode": "split_half"   
+     
+        }  
      },
      "ASCOT": {
         "single": {
